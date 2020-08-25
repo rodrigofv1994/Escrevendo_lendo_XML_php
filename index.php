e30a88be863a3945829862b2bbb80ba5e2226ad7
Rodrigo Ferreira de Vasconcelos Guimarães <rvasconcelos@outlook.com.br>
2020-08-25 16:16:09 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php
+//index.php
+session_start();
+
+if(isset($_GET['deslogar'])){
+      session_destroy(); 
+      echo "deslogar setado";
+}
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>Usuário:admin Senha:admin</p>
+        <form action="inicio.php" method="post">
+            <input type="text" name="usuario" required="required" maxlength="16" placeholder="Usuário" autocomplete="on">
+            <input type="password" name="senha" required="required" maxlength="16" placeholder="Senha" autocomplete="on">
+            <button type="submit">Logar</button>
+        </form>
+    </body>
+</html>
